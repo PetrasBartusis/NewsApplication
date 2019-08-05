362d4b48f5f0ca3360fdfb3af7ed6fe7ee7a85ad
PBartusis <petras.bartusis@voicehit.com>
2019-08-05 20:17:54 +0300

    Add NewsListViewModel

---
+++ app/src/main/java/com/example/newsapplication/main/newslist/NewsListViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.newsapplication.main.newslist
+
+import com.example.newsapplication.utils.viewmodel.BaseViewModel
+import javax.inject.Inject
+
+class NewsListViewModel @Inject constructor(
+
+) : BaseViewModel() {
+    override fun onCreated() {
+
+    }
+}

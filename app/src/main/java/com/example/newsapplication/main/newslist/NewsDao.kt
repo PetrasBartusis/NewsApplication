cfd149b4107f1d790c0b55ecf19446de4781ce0e
PBartusis <petras.bartusis@voicehit.com>
2019-08-05 23:21:43 +0300

    Add NewsDao

---
+++ app/src/main/java/com/example/newsapplication/main/newslist/NewsDao.kt
@@ -0,0 +1,15 @@
+package com.example.newsapplication.main.newslist
+
+import com.example.newsapplication.main.entities.News
+import com.example.newsapplication.utils.network.NewsApi
+import com.example.newsapplication.utils.scheduler.IOScheduler
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import javax.inject.Inject
+
+class NewsDao @Inject constructor(
+    private val newsApi: NewsApi,
+    @IOScheduler private val sheduler: Scheduler
+) {
+    fun getNews(): Single<News> = newsApi.getNews().subscribeOn(sheduler)
+}

d7e94f70adbdde49a427eea5c392eb2305d8c8b8
PBartusis <petras.bartusis@voicehit.com>
2019-08-05 22:24:56 +0300

    Add Activity Show Toast Extension

--- app/src/main/java/com/example/newsapplication/main/MainActivity.kt
+++ app/src/main/java/com/example/newsapplication/main/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import com.example.newsapplication.R
 import com.example.newsapplication.main.newslist.NewsListFragment
 import com.example.newsapplication.utils.activity.openFragment
+import com.google.android.material.snackbar.Snackbar
 import dagger.android.support.DaggerAppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         openFragment(R.id.container, NewsListFragment.newInstance(), false)
+    }
+
+    fun showMessage(message: String) {
+        Snackbar.make(container, message, Snackbar.LENGTH_LONG).show()
     }
 }

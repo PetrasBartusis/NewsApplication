1cf19b1ea61b3a91b4e2e50947238c8e8b6f22ba
PBartusis <petras.bartusis@voicehit.com>
2019-08-05 22:20:17 +0300

    Update NewsListViewModel To Show Articles

--- app/src/main/java/com/example/newsapplication/main/newslist/NewsListViewModel.kt
+++ app/src/main/java/com/example/newsapplication/main/newslist/NewsListViewModel.kt
@@ -1,12 +1,27 @@
 package com.example.newsapplication.main.newslist
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.example.newsapplication.main.entities.Article
 import com.example.newsapplication.utils.viewmodel.BaseViewModel
+import com.example.newsapplication.utils.viewmodel.SingleLiveData
 import javax.inject.Inject
 
 class NewsListViewModel @Inject constructor(
-
+        private val getNewsUseCase: GetNewsUseCase
 ) : BaseViewModel() {
-    override fun onCreated() {
+    private val articleList = MutableLiveData<List<Article>>()
+    private val message = SingleLiveData<String>()
 
+    fun getArticleList(): LiveData<List<Article>> = articleList
+
+    fun showErrorMessage(): LiveData<String> = message
+
+    override fun onCreated() {
+        getNewsUseCase.getNewsList().subscribe({ news ->
+            articleList.postValue(news.articles)
+        }, { error ->
+            message.postValue(error.message)
+        }).attachToViewModel()
     }
 }

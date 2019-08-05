dbcc2d457d05604cdc420826ae7304cdf0a90a82
PBartusis <petras.bartusis@voicehit.com>
2019-08-05 20:47:19 +0300

    Add GetNewsListUseCase

---
+++ app/src/main/java/com/example/newsapplication/main/newslist/GetNewsListUseCase.kt
@@ -0,0 +1,9 @@
+package com.example.newsapplication.main.newslist
+
+import javax.inject.Inject
+
+class GetNewsListUseCase @Inject constructor(
+        private val newListRepository: NewsListRepository
+) {
+
+}

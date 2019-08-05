2d0d4846d7c1da50ec188e6a6d6a505fd3e64bef
PBartusis <petras.bartusis@voicehit.com>
2019-08-06 00:05:31 +0300

    Pass Date Formatter To ArticleViewHolder

--- app/src/main/java/com/example/newsapplication/main/newslist/ArticleViewHolder.kt
+++ app/src/main/java/com/example/newsapplication/main/newslist/ArticleViewHolder.kt
@@ -3,13 +3,15 @@ package com.example.newsapplication.main.newslist
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.example.newsapplication.main.entities.Article
+import com.example.newsapplication.utils.dateformatter.DateFormatter
 import com.example.newsapplication.utils.images.ImageLoader
 import kotlinx.android.synthetic.main.item_article.view.*
 
 class ArticleViewHolder(
     itemView: View,
     private val onClick: (article: Article) -> Unit,
-    private val imageLoader: ImageLoader
+    private val imageLoader: ImageLoader,
+    private val dateFormatter: DateFormatter
 ): RecyclerView.ViewHolder(itemView) {
     fun bind(article: Article) {
         itemView.articleTitleTextView.text = article.title
--- app/src/main/java/com/example/newsapplication/main/newslist/NewsListFragment.kt
+++ app/src/main/java/com/example/newsapplication/main/newslist/NewsListFragment.kt
@@ -5,6 +5,7 @@ import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapplication.R
 import com.example.newsapplication.utils.activity.showMessage
+import com.example.newsapplication.utils.dateformatter.DateFormatter
 import com.example.newsapplication.utils.fragment.ViewModelFragment
 import com.example.newsapplication.utils.images.ImageLoader
 import kotlinx.android.synthetic.main.fragment_news_list.*
@@ -14,12 +15,19 @@ class NewsListFragment : ViewModelFragment() {
     @Inject
     lateinit var imageLoader: ImageLoader
 
+    @Inject
+    lateinit var dataFormatter: DateFormatter
+
     private val viewModel by viewModel<NewsListViewModel>()
 
     override val layoutRes = R.layout.fragment_news_list
 
     private val articleAdapter by lazy {
-        ArticleAdapter(onClick = {}, imageLoader = imageLoader)
+        ArticleAdapter(
+                onClick = {},
+                imageLoader = imageLoader,
+                dateFormatter = dataFormatter
+        )
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {

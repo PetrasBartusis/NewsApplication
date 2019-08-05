ffee9b20397b1b4267838a79d33a01e5ee4daca0
PBartusis <petras.bartusis@voicehit.com>
2019-08-05 20:54:47 +0300

    Add GetNewsListUseCase Method

--- app/src/main/java/com/example/newsapplication/main/newslist/GetNewsListUseCase.kt
+++ app/src/main/java/com/example/newsapplication/main/newslist/GetNewsListUseCase.kt
@@ -5,5 +5,5 @@ import javax.inject.Inject
 class GetNewsListUseCase @Inject constructor(
         private val newListRepository: NewsListRepository
 ) {
-
+    fun getNewsList() = newListRepository.getNewsList()
 }

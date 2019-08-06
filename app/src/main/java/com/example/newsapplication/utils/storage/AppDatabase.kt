094adaa52942481e78eda336dc6dfba22e1694c0
PBartusis <petras.bartusis@voicehit.com>
2019-08-06 20:11:52 +0300

    Add DatabaseArticle

--- app/src/main/java/com/example/newsapplication/utils/storage/AppDatabase.kt
+++ app/src/main/java/com/example/newsapplication/utils/storage/AppDatabase.kt
@@ -3,9 +3,9 @@ package com.example.newsapplication.utils.storage
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.newsapplication.utils.storage.daos.NewsDao
-import com.example.newsapplication.utils.storage.entities.ArticleEntity
+import com.example.newsapplication.utils.storage.entities.DatabaseArticle
 
-@Database(entities = [ArticleEntity::class], version = 1)
+@Database(entities = [DatabaseArticle::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun newsDao(): NewsDao
 }

d05beddbf7753d3fc5f6e740753e01933efe33d4
PBartusis <petras.bartusis@voicehit.com>
2019-08-06 21:12:35 +0300

    Implement SetNews In DefaultNewsRepository

--- app/src/main/java/com/example/newsapplication/main/newslist/DefaultNewsRepository.kt
+++ app/src/main/java/com/example/newsapplication/main/newslist/DefaultNewsRepository.kt
@@ -1,11 +1,22 @@
 package com.example.newsapplication.main.newslist
 
 import com.example.newsapplication.main.entities.News
+import com.example.newsapplication.utils.factory.ArticleFactory
+import com.example.newsapplication.utils.network.NewsService
+import com.example.newsapplication.utils.storage.NewsDataSource
 import io.reactivex.Single
 import javax.inject.Inject
 
 class DefaultNewsRepository @Inject constructor(
-    private val newsService: NewsService
+    private val newsService: NewsService,
+    private val newsDataSource: NewsDataSource,
+    private val articleFactory: ArticleFactory
 ) : NewsRepository {
-    override fun getNews(): Single<News> = newsService.getNews()
+    override fun getNews(): Single<News> {
+        return newsService.getNews()
+    }
+
+    override fun setNews(news: News) = newsDataSource.setNews(
+        articleFactory.getDatabaseArticles(news)
+    )
 }

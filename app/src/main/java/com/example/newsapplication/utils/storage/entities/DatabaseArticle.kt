7ee8c11070ba4a45812808289b279cfac55c0de0
PBartusis <petras.bartusis@voicehit.com>
2019-08-06 21:46:17 +0300

    Reformat DatabaseArticle

--- app/src/main/java/com/example/newsapplication/utils/storage/entities/DatabaseArticle.kt
+++ app/src/main/java/com/example/newsapplication/utils/storage/entities/DatabaseArticle.kt
@@ -7,12 +7,12 @@ import java.io.Serializable
 
 @Entity
 data class DatabaseArticle(
-        @PrimaryKey(autoGenerate = true)  val id: Int = 0,
-        @ColumnInfo(name = "author") val author: String?,
-        @ColumnInfo(name = "title") val title: String,
-        @ColumnInfo(name = "description") val description: String,
-        @ColumnInfo(name = "url") val url: String,
-        @ColumnInfo(name = "urlToImage") val urlToImage: String?,
-        @ColumnInfo(name = "publishedAt") val publishedAt: String,
-        @ColumnInfo(name = "sourceName") val sourceName: String
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    @ColumnInfo(name = "author") val author: String?,
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "description") val description: String?,
+    @ColumnInfo(name = "url") val url: String,
+    @ColumnInfo(name = "urlToImage") val urlToImage: String?,
+    @ColumnInfo(name = "publishedAt") val publishedAt: String,
+    @ColumnInfo(name = "sourceName") val sourceName: String
 ) : Serializable

220b41034a2d321993861dbae8c127da55c3e95d
PBartusis <petras.bartusis@voicehit.com>
2019-08-07 23:40:46 +0300

    Fix SimpleDateFormat

--- app/src/main/java/com/example/newsapplication/utils/dateformatter/DateFormatter.kt
+++ app/src/main/java/com/example/newsapplication/utils/dateformatter/DateFormatter.kt
@@ -5,7 +5,7 @@ import java.util.*
 import javax.inject.Inject
 
 class DateFormatter @Inject constructor() {
-    private val dateTimeFormat = SimpleDateFormat("yyyy-MM-dd hh:mm", Locale.getDefault())
+    private val dateTimeFormat = SimpleDateFormat("yyyy-MMMM-dd HH:mm", Locale.getDefault())
     private val startingFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
 
     fun getDateTimeFromDateString(dateToFormat: String): String? {

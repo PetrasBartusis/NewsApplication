d52f4fc22dbfef1a36d29d1f20cf265c61fb054a
PBartusis <petras.bartusis@voicehit.com>
2019-08-07 19:30:25 +0300

    Add ArticleViewModel

---
+++ app/src/main/java/com/example/newsapplication/main/article/ArticleViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.newsapplication.main.article
+
+import com.example.newsapplication.utils.viewmodel.BaseViewModel
+import javax.inject.Inject
+
+class ArticleViewModel @Inject constructor() : BaseViewModel() {
+
+    override fun onCreated() {
+        super.onCreated()
+    }
+}

c7bbe3d6bd4af10bac91bfef2abdcc78485c24e7
PBartusis <petras.bartusis@voicehit.com>
2019-08-07 19:29:20 +0300

    Use Navigation In MainActivity

--- app/src/main/java/com/example/newsapplication/main/MainActivity.kt
+++ app/src/main/java/com/example/newsapplication/main/MainActivity.kt
@@ -2,8 +2,6 @@ package com.example.newsapplication.main
 
 import android.os.Bundle
 import com.example.newsapplication.R
-import com.example.newsapplication.main.newslist.NewsListFragment
-import com.example.newsapplication.utils.activity.openFragment
 import com.google.android.material.snackbar.Snackbar
 import dagger.android.support.DaggerAppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
@@ -12,7 +10,6 @@ class MainActivity : DaggerAppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        openFragment(R.id.container, NewsListFragment.newInstance(), false)
     }
 
     fun showMessage(message: String) {
